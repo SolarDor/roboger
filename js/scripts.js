6c15aed228db7130356706587831de7b3b1a0b65
CameronWill <cellardoorsolar@aol.com>
2020-08-21 10:02:00 -0700

    fix specs in TDD

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,27 @@
 // Test Driven Development:
-  // Test: The number 32 should be replaced with "Won't you be my neighbor?"
+  // Spec: The number 32 should be replaced with "Won't you be my neighbor?"
   // Input: 32
   // Output: "Won't you be my neighbor?"
 
-  // Test: Behavior: The number 21 should be replaced with "Boop".
+  // Spec: Behavior: The number 21 should be replaced with "Boop".
   // Input: 21
   // Output: "Boop".
 
-  // Test: The number 13 should be replaced with "Won't you be my neighbor?"
+  // Spec: The number 13 should be replaced with "Won't you be my neighbor?"
   // Input: 13
   // Output: "Won't you be my neighbor?"
 
-  // Test: Numbers that contain a 3: all digits are replaced (all digits) with "Won't you be my neighbor?"
+  // Spec: Numbers that contain a 3: all digits are replaced (all digits) with "Won't you be my neighbor?"
   // Input: 3
-  // Output: "Won't you be my neighbor?"
+  // Output: "0, Beep!, Boop!, Won't you be my neighbor?"
 
-  // Test: Numbers that contain a 2: all digits are replaced (all digits) with "Boop!"
+  // Spec: Numbers that contain a 2: all digits are replaced (all digits) with "Boop!"
   // Input: 2
-  // Output: "Boop!"
+  // Output: "0, Beep!, Boop!"
 
-  // Test: Numbers that contain a 1: all digits are replaced (all digits) with "Beep!"
+  // Spec: Numbers that contain a 1: all digits are replaced (all digits) with "Beep!"
   // Input: 1
-  // Output: "Beep!"
+  // Output: "0, Beep!"
 
 // Business Interface
 
