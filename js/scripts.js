4a63e4a07fbbd4df4d06bcf5a822ec69a8ee9c8d
CameronWill <cellardoorsolar@aol.com>
2020-08-21 09:54:32 -0700

    add test driven development to scripts in comment

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+// Test Driven Development:
+  // Test: The number 32 should be replaced with "Won't you be my neighbor?"
+  // Input: 32
+  // Output: "Won't you be my neighbor?"
+
+  // Test: Behavior: The number 21 should be replaced with "Boop".
+  // Input: 21
+  // Output: "Boop".
+
+  // Test: The number 13 should be replaced with "Won't you be my neighbor?"
+  // Input: 13
+  // Output: "Won't you be my neighbor?"
+
+  // Test: Numbers that contain a 3: all digits are replaced (all digits) with "Won't you be my neighbor?"
+  // Input: 3
+  // Output: "Won't you be my neighbor?"
+
+  // Test: Numbers that contain a 2: all digits are replaced (all digits) with "Boop!"
+  // Input: 2
+  // Output: "Boop!"
+
+  // Test: Numbers that contain a 1: all digits are replaced (all digits) with "Beep!"
+  // Input: 1
+  // Output: "Beep!"
+
+// Business Interface
+
+// User Interface
